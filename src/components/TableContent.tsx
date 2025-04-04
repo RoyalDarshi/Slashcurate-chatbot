11c2039a5a0b966994f3f2264351775720bab4bb
RoyalDarshi <priyadarshiroy92@gmail.com>
2025-04-04 11:16:25 +0000

    deleted unnecessary comp

--- src/components/TableContent.tsx
+++
@@ -1,9 +0,0 @@
-// TableContent.jsx
-import React from "react";
-import DataTable from "./DataTable";
-
-const TableContent = ({ data }) => {
-  return <DataTable data={data} />;
-};
-
-export default TableContent;
